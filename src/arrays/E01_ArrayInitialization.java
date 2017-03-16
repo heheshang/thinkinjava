57107552d3cb62f827373a2cf3e6f5c7b09391b0
heheshang <2448318433@qq.com>
2017-03-16 14:02:38 +0800

    初始导入

---
+++ src/arrays/E01_ArrayInitialization.java
@@ -0,0 +1,27 @@
+package arrays;
+
+/**
+ * Created by Administrator on 2017/2/28 0028.
+ */
+public class E01_ArrayInitialization {
+    static void hide(BerylliumSphere[] s){
+        System.out.println("Hiding " + s.length + " sphere(s)");
+    }
+
+    public static void main(String[] args){
+// Dynamic aggregate initialization:
+        hide(new BerylliumSphere[]{new BerylliumSphere(),
+                new BerylliumSphere()});
+// The following line produces a compilation error.
+//! hide({ new BerylliumSphere() });
+// Aggregate initialization:
+        BerylliumSphere[] d = {new BerylliumSphere(),
+                new BerylliumSphere(), new BerylliumSphere()};
+        hide(d);
+// Dynamic aggregate initialization is redundant
+// in the next case:
+        BerylliumSphere[] a = new BerylliumSphere[]{
+                new BerylliumSphere(), new BerylliumSphere()};
+        hide(a);
+    }
+}
